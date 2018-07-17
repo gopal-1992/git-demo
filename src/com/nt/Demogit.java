80104e57e7de7d7085fe92220443638560bf46c1
gopal <mohankudagopal@gmail.com>
2018-07-17 12:41:32 +0530

    added git project to local repository

---
+++ src/com/nt/Demogit.java
@@ -0,0 +1,11 @@
+package com.nt;
+
+public class Demogit {
+public static void main(String[] args) {
+	System.out.println("hello");
+}
+public void test(String Data){
+	System.out.println(Data+" welcome");
+	
+}
+}
