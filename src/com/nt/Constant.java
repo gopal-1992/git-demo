2ffc9340140ea19b592bc992b0e36a090efbfa8b
gopal <mohankudagopal@gmail.com>
2018-07-17 13:25:32 +0530

    added new constant class on project

---
+++ src/com/nt/Constant.java
@@ -0,0 +1,5 @@
+package com.nt;
+
+public class Constant {
+public static String DATA_TYPE="do more hardwork gopal";
+}
--- src/com/nt/Demogit.java
+++ src/com/nt/Demogit.java
@@ -1,14 +1,15 @@
 package com.nt;
 
 public class Demogit {
-public static void main(String[] args) {
-	System.out.println("hello");
-	
-}
-public void test(String Data){
-	System.out.println(Data+" welcome");
-	if(Data.equals("BELIVE IN U R SELF GOPAL")){
-		System.out.println("grt job gopal");
+
+	public void test(String Data) {
+
+		if (Data.equals(Constant.DATA_TYPE)) {
+			System.out.println("grt job gopal");
+		}
 	}
-}
+
+	public static void main(String[] args) {
+		System.out.println("main method");
+	}
 }
