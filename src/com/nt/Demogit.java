9ccc22c2eb6295a6cfaddc7cd38fcf3e03ebbfbe
gopal <mohankudagopal@gmail.com>
2018-07-17 12:47:53 +0530

    added if condtion to cheack the data

--- src/com/nt/Demogit.java
+++ src/com/nt/Demogit.java
@@ -3,9 +3,12 @@ package com.nt;
 public class Demogit {
 public static void main(String[] args) {
 	System.out.println("hello");
+	
 }
 public void test(String Data){
 	System.out.println(Data+" welcome");
-	
+	if(Data.equals("BELIVE IN U R SELF GOPAL")){
+		System.out.println("grt job gopal");
+	}
 }
 }
